bfd479e6e4182aa67f779f033a5486567f2d49e0
fernandaaleixo246-art <fernandaaleixo246@gmail.com>
2026-04-23 22:47:41 -0300

    Add files via upload

---
+++ Potencia/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Potencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, resultado;  
+
+
+            Console.WriteLine("Digite o valor de x:");
+            x = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Digite o valor de y:");
+            y =  double.Parse(Console.ReadLine());
+
+            resultado= Math.Pow(x, y);
+
+            Console.WriteLine("Resultado> " + resultado);
+
+
+        }
+    }
+}
