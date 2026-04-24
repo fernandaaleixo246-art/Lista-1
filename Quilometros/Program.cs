0d774c8a908877bb8352284e5b609b900d8e18a3
fernandaaleixo246-art <fernandaaleixo246@gmail.com>
2026-04-23 22:25:27 -0300

    Add files via upload

---
+++ Quilometros/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quilometros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double milhas, km;
+
+            Console.WriteLine("Digite o valor em milhas marítimas");
+            milhas = Convert.ToDouble(Console.ReadLine());
+
+         
+
+           
+             km = milhas * 1852;
+
+            Console.WriteLine("Valor em quilometros:" + km);
+
+
+
+        }
+    }
+}
