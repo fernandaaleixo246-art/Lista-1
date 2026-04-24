604e0566418039f685168737eb3edd5f045c3f5d
fernandaaleixo246-art <fernandaaleixo246@gmail.com>
2026-04-23 22:11:45 -0300

    Add files via upload

---
+++ Média/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Média
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {double n1, n2, media;
+
+            Console.WriteLine("Digite a primeira nota:");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a segunda nota");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            media = (n2 + n1) / 2;
+
+            Console.WriteLine("Média = " + media);
+
+        }
+    }
+}
