f80bf14e7ca6942f6284539f907a81c58e3d9290
fernandaaleixo246-art <fernandaaleixo246@gmail.com>
2026-04-23 22:04:42 -0300

    Add files via upload

---
+++ Triangulo_/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseTriangulo, altura, area;
+
+            Console.WriteLine("Digite a base;");
+            baseTriangulo= double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura:");
+            altura = double.Parse(Console.ReadLine());
+
+            area = (baseTriangulo * altura / 2);
+
+
+            Console.WriteLine("Área =" + area); 
+
+
+
+
+
+        }
+    }
+}
