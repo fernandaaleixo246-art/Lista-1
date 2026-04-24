65ad20ca2688648975122bb643c85a9af36f2977
fernandaaleixo246-art <fernandaaleixo246@gmail.com>
2026-04-23 21:27:15 -0300

    Add files via upload

---
+++ Diagonal/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double diagonal, area;
+            Console.WriteLine("Digite o valor da diagonal do quadrado");
+            diagonal=double.Parse(Console.ReadLine());
+
+            area = (diagonal * diagonal) / 2;
+
+            Console.WriteLine("area = " + area) ;
+
+
+
+
+        }
+    }
+}
